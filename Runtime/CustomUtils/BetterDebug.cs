696117c8c7d3f84ea440444c3968e55cee860152
mattrt13 <mehdiroozitalab13@gmail.com>
2024-07-31 12:47:04 +0330

    shows where it got called

--- Runtime/CustomUtils/BetterDebug.cs
+++ Runtime/CustomUtils/BetterDebug.cs
@@ -9,8 +9,13 @@ namespace Runtime.Utils
         public static void Log(object text, Color color = default)
         {
             if (!canLog) return;
+
+            StackTrace stackTrace = new StackTrace(true);
+            StackFrame frame = stackTrace.GetFrame(1);
+            string callingMethod = $"{frame.GetFileName()}:{frame.GetFileLineNumber()}";
+
             var colorHex = color == default ? Color.white.GetHex() : color.GetHex();
-            Debug.Log($"<color={colorHex}>{text}</color>");
+            Debug.Log($"<color={colorHex}>{text}</color>\n{callingMethod}");
         }
     }
 }
