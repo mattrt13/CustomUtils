d3a3c9e58c77dcd7c0cf99c1a1e07701f3bed57f
mattrt13 <mehdiroozitalab13@gmail.com>
2024-07-31 13:45:27 +0330

    repushing (is that a word?)

---
+++ Runtime/CustomUtils/BetterDebug.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using Color = UnityEngine.Color;
+
+namespace Runtime.Utils
+{
+    public static class BetterDebug
+    {
+        private static bool canLog = true;
+        public static void CanLog(bool state) => canLog = state;
+        public static void Log(object text, Color color = default)
+        {
+            if (!canLog) return;
+
+            StackTrace stackTrace = new StackTrace(true);
+            StackFrame frame = stackTrace.GetFrame(1);
+            string callingMethod = $"{frame.GetFileName()}:{frame.GetFileLineNumber()}";
+
+            var colorHex = color == default ? Color.white.GetHex() : color.GetHex();
+            Debug.Log($"<color={colorHex}>{text}</color>\n{callingMethod}");
+        }
+    }
+}
